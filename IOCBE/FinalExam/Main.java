0da6a1795cfe117a7f948614162843b587f457e2
hoangnguyenle0703-py <hoangnguyenle0703@gmail.com>
2026-03-08 15:39:19 +0700

    Update Main.java

--- IOCBE/FinalExam/Main.java
+++ IOCBE/FinalExam/Main.java
@@ -2,7 +2,6 @@ import presentation.MainView;
 
 public class Main {
     public static void main(String[] args) {
-        // Khởi tạo và chạy giao diện chính
         MainView mainView = new MainView();
         mainView.start();
     }
